2dd708d1f97d3b9a5645aa675bcc017de75fbf25
matias12131 <velazquezmatiasclass@gmail.com>
2026-07-03 04:59:26 -0300

    Cliente: controllers, model, routes

---
+++ src/models/venta.model.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('./../../config/database'); // Asegúrate de que la ruta apunte a tu archivo
+const Venta = sequelize.define('Venta', {
+// Sequelize crea un campo 'id' autoincrementable automáticamente, no hace falta ponerlo
+    metodoPago: {type: DataTypes.ENUM('efectivo', 'transferencia', 'tarjeta', 'financiado'), allowNull: false},
+    cuotas: {type: DataTypes.INTEGER, allowNull: true},
+    descuento: {type: DataTypes.FLOAT, allowNull: true},
+    observaciones: {type: DataTypes.TEXT, allowNull: true},
+    fechaVenta: {type: DataTypes.DATE, allowNull: true},
+    precioFinal: {type: DataTypes.FLOAT, allowNull: true}
+}, {
+    tableName: 'ventas', // Nombre de la tabla en minúsculas y plural
+    timestamps: true, // Crea automáticamente los campos createdAt y updatedAt
+});
+
+
+
+module.exports = Venta;
