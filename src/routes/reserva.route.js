6daed23c63f314b0de3e1c0a5354fd4bf31af5fe
matias12131 <velazquezmatiasclass@gmail.com>
2026-07-05 22:24:06 -0300

    correccion de errores en el venta.controller y reserva.route

--- src/routes/reserva.route.js
+++ src/routes/reserva.route.js
@@ -1,11 +1,12 @@
 const reservaCtrl = require('../controllers/reserva.controller');
 const express = require('express');
 const router = express.Router();
+const authCtrl = require('../controllers/auth.controller');
 
 // Rutas para la gestión de Reservas
-router.get('/', reservaCtrl.getReservas);
-router.get('/:id', reservaCtrl.getReserva);
-router.post('/', reservaCtrl.createReserva);
-router.put('/:id/procesar', reservaCtrl.procesarReserva);
+router.get('/', authCtrl.verifyToken, authCtrl.verifyRole("vendedor", "admin"), reservaCtrl.getReservas);
+router.get('/:id', authCtrl.verifyToken, reservaCtrl.getReserva);
+router.post('/', authCtrl.verifyToken, reservaCtrl.createReserva);
+router.put('/:id/procesar', authCtrl.verifyToken, authCtrl.verifyRole("vendedor", "admin"), reservaCtrl.procesarReserva);
 
 module.exports = router;
